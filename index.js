6d6baa63163da92404924e8847c47af24f01becc
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-09-19 13:21:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100);
+
+const num3 = 6;
+const num4 = 28;
+
+const mod = num4 % num3;
+const max = Math.max(20, 8, 5, 1);
